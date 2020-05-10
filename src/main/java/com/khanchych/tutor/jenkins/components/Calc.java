8e01c4491acaec1fd744715db9dbfe1c3160b71c
rkhanchych <Roman.Khanchych@contractor.cengage.com>
2020-05-11 00:03:34 +0300

    adds Calc component

---
+++ src/main/java/com/khanchych/tutor/jenkins/components/Calc.java
@@ -0,0 +1,11 @@
+package com.khanchych.tutor.jenkins.components;
+
+public class Calc {
+    public int add(int a, int b) {
+        return (a + b);
+    }
+
+    public int sub(int a, int b) {
+        return (a - b);
+    }
+}
